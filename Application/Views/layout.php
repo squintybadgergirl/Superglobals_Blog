04693a8946c6f26f355ebaf9f8a529ae4aabb370
Ceri-anne Jackson <ceri@ceri-anne.co.uk>
2016-07-30 13:46:49 +0100

    Added Abstract View class and layout file

--- Application/Views/layout.php
+++ Application/Views/layout.php
@@ -1,8 +1,11 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1><?php
+             echo $heading;
+        ?><h2>
+    </body>
+</html>
