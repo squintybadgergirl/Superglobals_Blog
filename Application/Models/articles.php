72df9183222e52834fbbb78e2822ac986d36e11c
Ceri-anne Jackson <ceri@ceri-anne.co.uk>
2016-07-30 12:08:07 +0100

    Set up folder structure

---
+++ Application/Models/articles.php
@@ -0,0 +1,18 @@
+<?php
+
+class ArticleModel {
+
+    private $id;
+    private $title;
+    private $body;
+    private $author;
+    private $category;
+    private $creationdate;
+    private $modifieddate;
+    private $status;
+    
+    function __construct() {
+        
+    }
+    
+}
---
+++ Application/Models/users.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserModel {
+    
+    private $id;
+    private $username;
+    private $password;
+    private $fullname;
+    private $role;
+    private $email;
+    private $dateofbirth;
+    private $creationdate;
+    
+    function __construct() {
+        
+    }
+    
+}
+
